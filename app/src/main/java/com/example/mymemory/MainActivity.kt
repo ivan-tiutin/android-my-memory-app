b4fff8fd0a385d14a8e5c1af3572720be43119c5
Ivan Tiutin <tyutin.iv95@gmail.com>
2023-12-05 10:05:58 +0530

    Init a project and initial set up of it

---
+++ app/src/main/java/com/example/mymemory/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mymemory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumberMoves: TextView
+    private lateinit var tvNumberPairs: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumberMoves = findViewById(R.id.tvNumberMoves)
+        tvNumberPairs = findViewById(R.id.tvNumberPairs)
+
+    }
+}
