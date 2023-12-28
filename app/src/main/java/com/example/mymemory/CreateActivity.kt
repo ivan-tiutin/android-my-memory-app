046edb47d8b491da94f52285bfbf5ea8829f4550
Ivan Tiutin <tyutin.iv95@gmail.com>
2023-12-28 10:07:21 +0600

    Adjusted create activity

--- app/src/main/java/com/example/mymemory/CreateActivity.kt
+++ app/src/main/java/com/example/mymemory/CreateActivity.kt
@@ -2,16 +2,29 @@ package com.example.mymemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.mymemory.models.BoardSize
 import com.example.mymemory.utils.EXTRA_BOARD_SIZE
 
 class CreateActivity : AppCompatActivity() {
 
     private lateinit var boardSize: BoardSize
+    private var numImagesRequired = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
-        boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE, BoardSize::class.java)!!
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
+        numImagesRequired = boardSize.getNumPairs()
+        supportActionBar?.title =  "Chose pics (0 / $numImagesRequired)"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
